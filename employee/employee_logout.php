e6a93e76b299ff1aecfa667374f172715f893284
Tzu-Han Lin <lintzuh@kean.edu@obi2.kean.edu>
2023-11-04 23:16:38 -0400

    logout func

--- employee/employee_logout.php
+++ employee/employee_logout.php
@@ -1,7 +1,16 @@
 <?php
-include helperFunction;
-
+include 'helperFunction.php';
 deleteEmployeeCookie();
-
-
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+    <p>You have been logged out. Redirecting to the home page in 3 seconds...</p>
+
+    <script>
+        setTimeout(function() {
+            window.location.href = '../index.html';
+        }, 3000); // 3000 milliseconds = 3 seconds
+    </script>
+</body>
+</html>
